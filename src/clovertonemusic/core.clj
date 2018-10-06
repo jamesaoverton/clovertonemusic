e4540ea8e642e8117aedb212a38eae79042ec5e6
Michael Cuffaro <mike@gwydion>
2018-10-07 01:18:53 +0200

    add some minimal exception handling, and put catalogue data into one dictionary rather than separate data structures

--- src/clovertonemusic/core.clj
+++ src/clovertonemusic/core.clj
@@ -18,14 +18,22 @@
   [[header-row & body-rows]]
   (reduce
    (fn [saved-rows next-row]
-     (conj saved-rows (zipmap (map keyword header-row) next-row)))
+     (if (not= (count header-row) (count next-row))
+       (throw (Exception. "Number of fields in data row does not match the number of headers"))
+       (conj saved-rows (zipmap (map keyword header-row) next-row))))
    [] body-rows))
 
 (defn -main
   "<Description to be filled in>"
   [& args]
-  (def charts (create-table (extract-from-csv "charts.csv")))
-  (def composers (create-table (extract-from-csv "composers.csv")))
-  (def genres (create-table (extract-from-csv "genres.csv")))
-  (def grades (create-table (extract-from-csv "grades.csv")))
-)
+  (def catalogue
+    (reduce
+     (fn [result-map next-key]
+       (try
+         (assoc result-map
+                (keyword next-key)
+                (create-table (extract-from-csv (str next-key ".csv"))))
+         (catch Exception ex
+           (println (str "Error while parsing " next-key ".csv: " (.getMessage ex)))
+           (System/exit 1))))
+     {} ["charts" "composers" "genres" "grades"])))
