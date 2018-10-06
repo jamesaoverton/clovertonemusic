397403ee1e451eecf3b4e52a8bc9d0fcd8383ac1
Michael Cuffaro <mike@gwydion>
2018-10-06 11:21:28 +0200

    change license info in project file

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clovertonemusic "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "TBD"
+            :url "TBD"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.4"]]
   :main ^:skip-aot clovertonemusic.core
