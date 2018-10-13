d4a63d858f1524b31c55d7925116a9008abfe746
Michael Cuffaro <mike@gwydion>
2018-10-13 21:08:17 +0200

    add row number in log out put when failing during validation

--- src/clovertonemusic/core.clj
+++ src/clovertonemusic/core.clj
@@ -94,12 +94,12 @@
    (1) Validate that it is not empty if it is a required field
    (2) Validate that the contents conform to the column's datatype
    (3) If the column has a foreign key, then check in the catalogue to see that it is satisfied"
-  [table column contents catalogue]
+  [table rownum column contents catalogue]
   ; split up the string specifying the constraints associated with this column (defined above):
   (let [[required datatype foreign-key] (str/split (get (get catalogue-table-constraints table) column) #"/")]
     ; If the column is required, make sure it is not empty:
     (when (and (= required "y") (not (re-find #"\S+" contents)))
-      (fail (str "Required column: " column " of table " table " is empty")))
+      (fail (str "At row " rownum ": Required column: " column " of table " table " is empty")))
     ; Make sure the contents conform to the column's datatype:
     (when (or
            (and (= datatype "datetime") (not (re-matches
@@ -109,7 +109,7 @@
            (and (= datatype "money") (not (re-matches #"\s*(\$\d+(\.\d+){0,1}){0,1}\s*" contents)))
            (and (= datatype "ratio") (not (re-matches #"\s*(\d+/\d+){0,1}\s*" contents)))
            (and (= datatype "number") (not (re-matches #"\s*\d*\s*" contents))))
-      (fail (str "'" contents "' is not a valid " datatype " in column '" column "' of table '" table "'")))
+      (fail (str "At row " rownum ": '" contents "' is not a valid " datatype " in column '" column "' of table '" table "'")))
     ; Validate the foreign key if it exists:
     (when-not (nil? foreign-key)
       (let [[foreign-table foreign-column]
@@ -125,20 +125,21 @@
                                 (conj result-vector (get next-row foreign-column)))
                               [] (get catalogue foreign-table))]
           (when-not (some #(= contents %) foreign-values)
-            (fail (str "'" contents "' not in " foreign-values))))))))
+            (fail (str "At row " rownum ": '" contents "' not in " foreign-values))))))))
 
 (defn validate-table
   "Validates the data in the given table, row by row, column by column, using the catalogue
   to validate any foreign keys"
   [table catalogue]
   (log/info "Validating" (name table))
-  (loop [[curr-row & remaining-rows] (get catalogue table)]
+  (loop [[curr-row & remaining-rows] (get catalogue table)
+         curr-rownum 1]
     (loop [[curr-col & remaining-cols] (keys curr-row)]
-      (validate-cell table curr-col (get curr-row curr-col) catalogue)
+      (validate-cell table curr-rownum curr-col (get curr-row curr-col) catalogue)
       (when-not (empty? remaining-cols)
         (recur remaining-cols)))
     (when-not (empty? remaining-rows)
-      (recur remaining-rows))))
+        (recur remaining-rows (inc curr-rownum)))))
 
 (defn -main
   "At startup, the server creates a map called `catalogue` which consists of four tables
