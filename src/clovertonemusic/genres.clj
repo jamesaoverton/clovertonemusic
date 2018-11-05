fc86d921744d50db22f3f515bfd963b78dca2a48
Michael Cuffaro <mike@gwydion>
2018-11-05 16:56:04 -0500

    subdivide grades page

---
+++ src/clovertonemusic/genres.clj
@@ -0,0 +1,7 @@
+(ns clovertonemusic.genres-page)
+
+(load "common-divs")
+(load "chart-divs")
+
+(require '[clovertonemusic.common-divs :as common-divs]
+         '[clovertonemusic.chart-divs :as chart-divs])
