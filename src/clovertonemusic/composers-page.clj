88a2196fe62795ae0d05434f59013fc7b88ffd4a
Michael Cuffaro <mike@gwydion>
2018-11-05 18:03:01 -0500

    add stub for composers

---
+++ src/clovertonemusic/composers-page.clj
@@ -0,0 +1,115 @@
+(ns clovertonemusic.genres-page)
+
+(load "chart-divs")
+
+(require '[clovertonemusic.chart-divs :as chart-divs])
+
+(def html-composers
+  {"alison-au" {:title "Allison Au - Clovertone Music"
+                :contents []
+                :charts [:div#charts
+                         [:div#list
+                          ]]
+                :users [:div#users]}
+   "andy-ballantyne" {:title ""
+                      :contents []
+                      :charts [:div#charts
+                               [:div#list
+                                ]]
+                      :users [:div#users]}
+   "brad-cheeseman" {:title ""
+                     :contents []
+                     :charts [:div#charts
+                              [:div#list
+                               ]]
+                     :users [:div#users]}
+   "brad-harrison" {:title ""
+                    :contents []
+                    :charts [:div#charts
+                             [:div#list
+                              ]]
+                    :users [:div#users]}
+   "chelsea-mcbride" {:title ""
+                      :contents []
+                      :charts [:div#charts
+                               [:div#list
+                                ]]
+                      :users [:div#users]}
+   "chris-hunt" {:title ""
+                 :contents []
+                 :charts [:div#charts
+                          [:div#list
+                           ]]
+                 :users [:div#users]}
+   "christian-overton" {:title ""
+                        :contents []
+                        :charts [:div#charts
+                                 [:div#list
+                                  ]]
+                        :users [:div#users]}
+   "erik-patterson" {:title ""
+                     :contents []
+                     :charts [:div#charts
+                              [:div#list
+                               ]]
+                     :users [:div#users]}
+   "ewan-divitt" {:title ""
+                  :contents []
+                  :charts [:div#charts
+                           [:div#list
+                            ]]
+                  :users [:div#users]}
+   "greg-crowe" {:title ""
+                 :contents []
+                 :charts [:div#charts
+                          [:div#list
+                           ]]
+                 :users [:div#users]}
+   "jason-logue" {:title ""
+                  :contents []
+                  :charts [:div#charts
+                           [:div#list
+                            ]]
+                  :users [:div#users]}
+   "jim-lewis" {:title ""
+                :contents []
+                :charts [:div#charts
+                         [:div#list
+                          ]]
+                :users [:div#users]}
+   "josh-grossman" {:title ""
+                    :contents []
+                    :charts [:div#charts
+                             [:div#list
+                              ]]
+                    :users [:div#users]}
+   "liam-gallagher" {:title ""
+                     :contents []
+                     :charts [:div#charts
+                              [:div#list
+                               ]]
+                     :users [:div#users]}
+   "maria-kundakcioglu" {:title ""
+                         :contents []
+                         :charts [:div#charts
+                                  [:div#list
+                                   ]]
+                         :users [:div#users]}
+   "michael-kundakcioglu" {:title ""
+                           :contents []
+                           :charts [:div#charts
+                                    [:div#list
+                                     ]]
+                           :users [:div#users]}
+   "terry-promane" {:title ""
+                    :contents []
+                    :charts [:div#charts
+                             [:div#list
+                              ]]
+                    :users [:div#users]}
+   "tom-richards" {:title ""
+                   :contents []
+                   :charts [:div#charts
+                            [:div#list
+                             ]]
+                   :users [:div#users]}})
